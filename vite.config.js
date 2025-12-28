5c75ac01740d4a0ca09ae0b1764134ef0f6757e5
AlexworlD2001 <roger.montejo@gmail.com>
2025-12-27 21:49:39 -0600

    Fix: Ajuste de base path para despliegue en Vercel

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // ** IMPORTANTE: Esta base asegura que GitHub Pages encuentre los archivos estáticos **
-  base: "/TVSA-Cotizador/", 
-  
+
   plugins: [react()],
 })
